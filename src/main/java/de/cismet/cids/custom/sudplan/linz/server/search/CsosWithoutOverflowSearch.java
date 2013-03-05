a0cdfa6bb8c85b1fd81ec3d3d4cb693004834712
Martin Scholl <martin.scholl@cismet.de>
2013-03-05 16:28:51 +0100

    linz pilot specific implementations moved to this separate project

---
+++ src/main/java/de/cismet/cids/custom/sudplan/linz/server/search/CsosWithoutOverflowSearch.java
@@ -0,0 +1,29 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package de.cismet.cids.custom.sudplan.linz.server.search;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   pd
+ * @version  $Revision$, $Date$
+ */
+public class CsosWithoutOverflowSearch extends CsoByOverflowSearch {
+
+    //~ Constructors -----------------------------------------------------------
+
+    /**
+     * Creates a new CsosWithoutOverflowSearch object.
+     *
+     * @param  swmmRunId  DOCUMENT ME!
+     */
+    public CsosWithoutOverflowSearch(final int swmmRunId) {
+        super(swmmRunId, 0.0f);
+        this.searchName = "csos-without-overflow-search";
+    }
+}
